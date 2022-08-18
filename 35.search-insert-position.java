ce601a7b31b7052e563d12b1cff06bac12f2fa69
Harry Zhang <harryzyz@gmail.com>
2022-08-18 00:30:07 -0500

    up

--- 35.search-insert-position.java
+++ 35.search-insert-position.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
  * @lc app=leetcode id=35 lang=java
  *
@@ -7,8 +9,20 @@
 // @lc code=start
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        int n = (int) Math.floor(nums.length / 2);
+        while (true) {
+            if (nums[n] == target) {
+                break;
+            } else if (nums.length == 1) {
+                n++;
+                break;
+            } else if (nums[n] > target) {
+                return searchInsert(Arrays.copyOfRange(nums, 0, n - 1), target);
+            } else if (nums[n] < target) {
+                return searchInsert(Arrays.copyOfRange(nums, n, nums.length - 1), target);
+            }
+        }
+        return (int) n;
     }
 }
 // @lc code=end
-
