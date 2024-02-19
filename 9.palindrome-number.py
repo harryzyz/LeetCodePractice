9e3251db374fa29fbb3d46e416753f2061d46b5a
Harry Zhang <harry.zhang@gatech.edu>
2024-02-19 12:22:48 -0800

    created a few questions

---
+++ 9.palindrome-number.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=9 lang=python
+#
+# [9] Palindrome Number
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x) == str(x)[::-1]:
+            return True
+        else:
+            return False
+        
+# @lc code=end
+
