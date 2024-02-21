896fd6c6057caae2dc97209c208c82ef3057830a
Harry Zhang <harry.zhang@gatech.edu>
2024-02-21 13:38:23 -0800

    up

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=88 lang=python
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        for idx2 in range(n):
+            for idx1 in range(m):
+                if nums2[idx2] < nums1[idx1]:
+                    # nums1.insert(idx1,nums2[idx2]).pop()
+                    nums1[idx1+1:] = nums1[idx1:-1]
+                    nums1[idx1] = nums2[idx2] 
+                    break                                 
+        return None
+        
+# @lc code=end
+
